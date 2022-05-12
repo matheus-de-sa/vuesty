427e007455ccedf5e7135d09a97f53bb3cc981dc
Matheus de Sá <matheuss_de_sa@hotmail.com>
2022-05-11 23:55:51 -0300

    methods button

--- src/components/styButton/styButton.vue
+++ src/components/styButton/styButton.vue
@@ -57,10 +57,6 @@ export default {
             type: Boolean,
             default: false
         },
-        relief: {
-            type: Boolean,
-            default: false
-        },
         shadow: {
             type: Boolean,
             defaul: false
@@ -81,7 +77,71 @@ export default {
     data() {
         return {}
     },
-    methods: {},
-    computed: {}
+    methods: {
+        clickButton(event) {
+            if (!this.disabled) this.$emit('click', event)
+        },
+        blurButton(event) {
+            if (!this.disabled) this.$emit('blur', event)
+        },
+        mouseover(event) {
+            if (!this.disabled) this.$emit('mouseover', event)
+        },
+        mouseout(event) {
+            if (!this.disabled) this.$emit('mouseout', event)
+        }
+    },
+    computed: {
+        isColor() {
+            if (this.color.includes(',') || this.color.includes('#'))
+                return false
+            else return true
+        },
+        stylesLoading() {
+            if (this.loading && this.flat && !this.isColor) {
+                return {
+                    color: `rgba(${this.color}, 1)`
+                }
+            }
+            return {}
+        },
+        styles() {
+            if (!this.isColor) {
+                if (this.border) {
+                    if (this.loading) {
+                        return {
+                            color: `rgba(${this.color}, 0)`,
+                            'background-color': `rgba(${this.color}, 0.2)`
+                        }
+                    } else {
+                        return {
+                            color: `rgba(${this.color}, 1)`,
+                            'background-color': `rgba(${this.color}, 0.2)`
+                        }
+                    }
+                } else {
+                    return {
+                        'background-color': this.color
+                    }
+                }
+            }
+            return null
+        },
+        type() {
+            let type = 'default'
+            if (this.rounded) type = 'rounded'
+            if (this.border) type = 'border'
+            return type
+        },
+        listeners() {
+            return {
+                ...this.$listeners,
+                click: (event) => this.clickButton(event),
+                blur: (event) => this.blurButton(event),
+                mouseover: (event) => this.mouseover(event),
+                mouseout: (event) => this.mouseout(event)
+            }
+        }
+    }
 }
 </script>
